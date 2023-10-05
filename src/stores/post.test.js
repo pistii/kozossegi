c4ac814b35e0c9588a7a3e2550471b42dcf3893b
pistii <pilisi.istvan@gmail.com>
2023-10-05 17:25:51 +0200

    test

---
+++ src/stores/post.test.js
@@ -0,0 +1,23 @@
+import { fetchData } from "./server_routes.js";
+
+async function postRegister(data) {
+    const response = await fetchData('register', data);
+    return response.status;
+}
+
+test("Returns response from server", async () => {
+    const data = {
+        firstName: "John",
+        middleName: "",
+        lastName: "Doe",
+        email: "johndoe@doe.com",
+        password: "123456",
+        birthDay: "1990-08-16",
+        PlaceOfResidence: "Vhol"
+    }
+    expect(postRegister(data)).resolves.toBe(200);
+});
+
+
+module.exports = postRegister;
+
