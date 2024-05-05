b3e79ed017ecaa6bbe9af124a32b79b82b559f2e
pistii <pilisi.istvan@gmail.com>
2024-05-05 19:06:56 +0200

    mobileView for navigation bar

---
+++ src/components/navBar/mobileView.vue
@@ -0,0 +1,63 @@
+<template>
+        <div class="flex-container" > <!--öröklött osztály a navBar-ból-->
+            <div v-if="isLoggedin()">
+                <v-col class="navBarMiddleIcons" > <!-- :style="{'width': 100% - txtAreaWidth + 'px'}" :cols="shouldExpandSearch ? '7' : '9'"-->
+                    <navButtons  />
+                </v-col>
+                <v-col cols="1" class="px-2" >
+                    <v-btn v-if="!this.shouldExpandSearch"
+                    class="pr-8" 
+                    @click="animateTxtBox()" 
+                    variant="plain" 
+                    icon="mdi-magnify" 
+                    style="font-size: 13px; padding: 18px"></v-btn>
+                    <navSearch v-else  :style="{'width': txtAreaWidth + 'px'}" style="float: right;"/>
+                </v-col>
+                <v-col cols="1" sm="2" xs="3">
+                    <navMenu />
+                </v-col>
+            </div>
+        </div>
+</template>
+
+<script>
+import navButtons from './navButtons.vue'
+import navMenu from './navMenu.vue'
+import navSearch from './navSearch.vue'
+import { isLoggedin } from '@/utils/auth.js';
+
+export default {
+    components: {
+        navSearch,
+        navButtons,
+        navMenu,
+    },
+    data() {
+        return {
+            shouldExpandSearch: false,
+            txtAreaWidth: 0,
+            isLoggedin,
+
+        }
+    },
+    methods: {
+        animateTxtBox() {
+            this.shouldExpandSearch = true;
+            const interval = setInterval(() => {
+            if (this.txtAreaWidth >= 200) {
+                clearInterval(interval);
+            } else {
+            this.txtAreaWidth += 5;
+            }
+        }, 10);
+        },
+        
+    },
+}
+</script>
+
+<style>
+.txtArea {
+    
+}
+</style>
