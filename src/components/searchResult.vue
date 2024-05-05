3b17b9349d397ccb94edb45e746fa7d1990d3da6
pistii <pilisi.istvan@gmail.com>
2024-05-05 18:50:56 +0200

    searchresult uses userCard component to display people result

--- src/components/searchResult.vue
+++ src/components/searchResult.vue
@@ -1,88 +1,42 @@
 <template>
     <v-row no-gutters>
-        <v-container class="d-flex flex-wrap " v-if="searchResult">
-            <v-col cols="4" v-for="user in searchResult[0]">
-                <v-layout>
-                <v-card color="#1b4e54" theme="dark" max-width="15rem" class="text-center pa-3 rounded">
-                    <v-card-title>
-                        <RouterLink :to="'/profile='+user.id" class="authorName">
-                            {{ user.firstName }} {{ user.lastName }}
-                        </RouterLink>
-                    </v-card-title>
-                        <v-avatar size="130" rounded="3">
-                            <v-img :src="getUserAvatar(user.avatar)" />
-                        </v-avatar>
-                        <v-card-title>
-                            <div style="font-family: Arial; font-size: 1.2vh;">Születési hely:</div>
-                            <div style="font-family: Arial; font-size: 1.2vh;">
-                                {{ user.placeOfBirth }}
-                            </div>
-                            <div>
-                                {{ getYears(user.dateOfBirth) }}
-                            </div>
-                        </v-card-title>
-                        <v-card-actions class="justify-center">
-                            <v-btn @click="addFriend(user)" variant="tonal" color="secondary" :text="friendRequestStatus[user.id] ?? 'Jelölés'"></v-btn>
-                            <v-progress-circular
-                                indeterminate
-                                color="primary"
-                            ></v-progress-circular>
-                        </v-card-actions>
-                    </v-card>
-            </v-layout>
+        <v-container class="d-flex flex-wrap " v-if="searchResult.length > 0">
+            <v-col cols="4" v-for="user in searchResult">
+                <UserCard :user="user" />
             </v-col>
-
         </v-container>
+
         <v-container v-else>
             Nincs a keresésnek megfelelő eredmény.
         </v-container>
     </v-row>
 </template>
 
-<script setup>
-import { peopleMatch } from '@/assets/navBar.vue';
-import { getUserAvatar } from '@/utils/common';
-import { reactive } from 'vue';
-import moment from 'moment';
-import store from '@/stores/UserStore';
-import fetchData from '@/stores/server_routes'
-
-var searchResult = peopleMatch;
-console.log(searchResult)
-</script>
-
 <script>
+import { reactive, ref } from 'vue';
+import eventBus from '@/stores/eventBus.js';
+import UserCard from '@/components/UserCard.vue';
+
+var searchResult = ref([]);
 
 export default {
+    components: {
+        UserCard,
+    },
+    mounted() {
+        eventBus.on('searchResult', (val) => {
+            searchResult.value = val
+        });
+    },
     data() {
         return {
             friendRequestStatus: reactive({}),
-            
+            searchResult,
         }
     },
-    methods: {
-        navigateToUserpage(id) {
-            this.$router.push("/profile=" + id);
-        },
-        getYears(birthYear) {
-            let birthDate = moment(birthYear);
-            let age = moment().diff(birthDate, 'years');
-            return age + ' éves';
-        },
-
-        async addFriend(user) {
-            var request = {
-                ApplicantId: store.getters.getUserId(),
-                toUserId: user.id,
-                NotificationType: 0
-            }
-            var resp = await fetchData.methods.fetchData('POST', 'PostFriendRequest', request);
-            if (resp == 'Success') {
-                console.log("ismerősnek jelölés elküldve");
-                this.friendRequestStatus[user.id] = 'Jelölve';
-                console.log(this.friendRequestStatus)
-            }
-        }
+    
+    beforeDestroy() {
+        eventBus.$off('searchResult')
     }
 }
 
