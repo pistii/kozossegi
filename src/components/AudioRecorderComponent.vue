8e8390ebbdf56f983e69427059c50b27deadb8b3
pistii <pilisi.istvan@gmail.com>
2024-05-15 23:17:24 +0200

    add audio recorder

---
+++ src/components/AudioRecorderComponent.vue
@@ -0,0 +1,44 @@
+<template>
+    <AudioRecorder 
+    v-if="MicrophoneMenuOpen" 
+    class="audioPopup"></AudioRecorder>
+    <v-icon class="onHover px-6 py-3 mb-2"  
+    @click="openMicrophoneDialog">
+    mdi-microphone</v-icon>
+</template>
+
+<script>
+import AudioRecorder from '@/views/Message/MessagePage/audioRecorder.vue'
+
+export default {
+    data() {
+        return {
+            MicrophoneMenuOpen: false,
+            newAudio: null,
+
+        }
+    },
+    components: {
+        AudioRecorder
+    },
+    methods: {
+        openMicrophoneDialog() {
+            this.MicrophoneMenuOpen = !this.MicrophoneMenuOpen;
+        },
+
+    }
+}
+</script>
+
+
+<style scoped>
+
+
+.audioPopup {
+    position: absolute;
+    bottom: 100px;
+    right: 0px;
+    min-width: 100%;
+    padding-left: 12px;
+}
+</style>
