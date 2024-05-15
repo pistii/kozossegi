dd498fcd3a8ae8553245697bf28a6b7aaa60fcc0
pistii <pilisi.istvan@gmail.com>
2024-05-15 23:17:33 +0200

    add emoji picker component

---
+++ src/components/EmojiPickerComponent.vue
@@ -0,0 +1,50 @@
+<template>
+    <EmojiPicker 
+        v-if="EmojiVisible" 
+        @emoji_click="handleEmojiClick" />
+        <v-icon 
+        class="px-6 py-3 mb-2"
+        @click="onEmojiPicker">mdi-emoticon</v-icon>
+</template>
+
+<script>
+import EmojiPicker from '@/views/Message/MessagePage/EmojiPicker.vue'
+import emojiJsonData from '@/emojis-data.json'
+import eventBus from '@/stores/eventBus';
+
+export default {
+    emits: ['selectedEmoji'],
+    data() {
+        return {
+            EmojiVisible: false,
+            
+            'Frequently used': {
+                emojiname: ''
+            },
+        }
+    },
+    components: {
+        EmojiPicker,
+    },
+    methods: {
+        onEmojiPicker() {
+            this.EmojiVisible = !this.EmojiVisible
+        },
+        handleEmojiClick(emoji) {
+            eventBus.emit('selectedEmoji', emoji); //Notify the Card Footer component about the added emoji
+            var keys = Object.keys(emojiJsonData['Frequently used'])
+            var items = Object.entries(emojiJsonData);
+            
+            //Todo: Store the recently used emojis in store
+            items[0][1]["emoji"] = emoji
+            for (var i = 0; i < items.length; i++) {
+                for (let key in items[i]) {
+                    items.keys += emoji
+                }
+            }
+            this.EmojiVisible = false;
+
+        },
+    }
+}
+</script>
