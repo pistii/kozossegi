36761b1bd576344a5d262a4bc357d6ed0f12db2c
pistii <pilisi.istvan@gmail.com>
2024-05-19 16:06:41 +0200

    textBox

---
+++ src/components/ChatCard/ChatTextBox.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-text-field
+        class=" bg-blue-lighten-4" 
+        placeholder="Say something..."
+        append-inner-icon="mdi-send-circle" 
+        v-model="message" 
+        @click:append-inner="onSendMessage"
+        v-on:keyup.enter="onSendMessage">
+    </v-text-field>
+</template>
+
+<script setup>
+import UserStore from '@/stores/UserStore';
+import fetchData from '@/stores/server_routes';
+import { ref } from 'vue';
+import eventBus from '@/stores/eventBus';
+import MessageStore from '@/stores/MessageStore';
+
+const message = ref('');
+
+const onSendMessage = async () => {
+    if (message.value.length > 0) {
+        var userId = UserStore.state.userId;
+        var partnerId = MessageStore.getters.getPartnerId();
+        let sendValue = {
+            "senderId": userId,
+            "authorId": userId,
+            "receiverId": partnerId,
+            "message": message,
+            "status": 1
+        }
+        await fetchData.methods.fetchData('POST', "PostChatMessage", sendValue)
+        .then(updateChatRoom(sendValue));
+        message.value = '';
+    }
+};
+const updateChatRoom = (data) => {
+    eventBus.emit('updateChatRoom', data)
+}
+</script>
