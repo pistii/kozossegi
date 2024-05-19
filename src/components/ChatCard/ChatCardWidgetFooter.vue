95a9298e7c2e6aec209ecb1545c2450fd1e26f07
pistii <pilisi.istvan@gmail.com>
2024-05-19 16:06:57 +0200

    Add footer

--- src/components/ChatCard/ChatCardWidgetFooter.vue
+++ src/components/ChatCard/ChatCardWidgetFooter.vue
@@ -1,104 +1,118 @@
 <template>
-    <div>
-        <EmojiPicker />
-        <AudioRecorder />        
+    <div class="chat-menu-icon-row"> <!--Ikonok elrendezése-->
+        <EmojiPicker :activeMenu="this.activeMenu"/>
+            <v-icon
+            :class="{ active: this.activeMenu == 'audio'}"
+            class="px-6 py-3 mb-2 menu" 
+                @click="toggleMenu('audio')"
+                >
+                mdi-microphone
+            </v-icon>
+            <v-icon  
+            :class="{ active: this.activeMenu == 'text'}"
+                class="px-6 py-3 mb-2 menu" 
+                @click="toggleMenu('text')">
+                mdi-keyboard
+            </v-icon>
+    </div>   
+    <div v-if="activeMenu=='audio'">
+        <AudioRecorder v-if="this.recording"/>
+        <div v-else>
+            <v-row no-gutters align="center">
+                <v-col>
+                    <audioVisualizer :totalWidth="250" class="audio-visualizer-container" :newAudio="this.newAudio" />
+                </v-col>
+                <v-col class="audio-visualizer-container justify-space-between">
+                    <v-tooltip text="Vissza" location="top" 
+                    open-delay="1000">
+                        <template v-slot:activator="{ props }">
+                            <v-btn class="w-100" size="x-small" v-bind="props">
+                                <v-icon size="20" tag="back" start="false" end="false">mdi-keyboard-backspace</v-icon>
+                            </v-btn>
+                        </template>
+                    </v-tooltip>
+                    
+                    <v-tooltip 
+                    text="Küldés" 
+                    location="top"
+                    open-delay="1000">
+                        <template v-slot:activator="{ props }">
+                            <v-container 
+                            fluid 
+                            class="onHover fill-height py-3 px-1" 
+                            fill-height 
+                            style="height: 100%;"
+                            >
+                                <v-icon 
+                                size="34" 
+                                color="green" 
+                                class="textbox-send-icon"
+                                v-bind="props"
+                                >mdi-send-circle</v-icon>
+                            </v-container>
+
+                        </template>
+                    </v-tooltip>
+                </v-col>
+                </v-row>
+        </div>
     </div>
-    <audioVisualizer v-if="newAudio" :newAudio="this.newAudio" />
+    <TextBox v-if="activeMenu=='text'"/>
     
-    <v-text-field  
-        class=" bg-blue-lighten-4" 
-        placeholder="Say something..."
-        append-inner-icon="mdi-send-circle" 
-        v-model="messageTo" 
-        @click:append-inner="onSendMessage"
-        v-on:keyup.enter="onSendMessage()">
-    </v-text-field>
 </template>
 
 <script>
-import { nextTick } from 'vue';
 import eventBus from '@/stores/eventBus';
 
-import UserStore from '@/stores/UserStore';
-import MessageStore from '@/stores/MessageStore';
-
-import fetchData from '@/stores/server_routes';
 import audioVisualizer from '@/components/audioVisualizer.vue';
 import EmojiPickerComponent from '../EmojiPickerComponent.vue';
-import AudioRecorder from '../AudioRecorderComponent.vue';
+import AudioRecorder from '@/views/Message/MessagePage/audioRecorder.vue';
+
+import TextBox from './ChatTextBox.vue'
 
     export default {
         components: {
             audioVisualizer,
             EmojiPicker: EmojiPickerComponent,
-            AudioRecorder
+            AudioRecorder,
+            TextBox,
+            
         },
         data() {
             return {
-                messageTo: '',
                 newAudio: null,
+                activeMenu: 'text',
+                recording: true,
             }
         },
         mounted() {
             eventBus.on('newAudioCreated', this.insertAudio)
             eventBus.on('selectedEmoji', this.addEmoji) //Subscribe to the emoji picker component
         },
-        methods: {                
-            async onSendMessage() {
-                if (this.messageTo.length > 0) {
-                    var userId = UserStore.state.userId;
-                    var partnerId = this.getPartnerId
-                    //console.log(userId)
-                    let sendValue = {
-                        "senderId": userId,
-                        "authorId": userId,
-                        "receiverId": partnerId,
-                        "message": this.messageTo,
-                        "status": 1
-                    }
-                    //console.log("partnerId" + partnerId)
-                    await fetchData.methods.fetchData('POST', "PostChatMessage", sendValue)
-                    .then(resp => this.afterMessageSent(sendValue, resp));
-                }
-            },
-
+        methods: {
             addEmoji(emoji) {
                 this.messageTo += emoji;
-            },
-            
-            afterMessageSent(message, resp) {
-                if (resp === 'Message sent') {
-                    MessageStore.commit('sendNewMessage', message); //TODO: Ezt az üzenetet a chatcontentbe beleteszi függetlenül attól hogy az adott content valóban a helyes szobába kerül-e. Ezt ellenőrizni kell, vagy a megfelelő szobába kell belehelyezni
-                    this.messageTo = '';
-                    this.stayBottom();
-                    this.updateChatRoomText(message);
-                }
-            },
-            
-            scrollToBottom() {
-                const container = this.$refs.parent;
-                if (container != null) {
-                    if (container.$el) {
-                        const scrollHeight = container.$el.scrollHeight;
-                        container.$el.scrollTo({ top: scrollHeight });
-                    }
-                }
-            },
-            stayBottom() {
-                nextTick(() => {
-                    this.scrollToBottom();
-                });
             },
 
             insertAudio(audio) {
                 this.newAudio = audio
+                this.activeMenu = 'audio'
+                this.recording = false;
             },
-            updateChatRoomText(message) {
-                //Eventbus
-                eventBus.emit('updateChatRoomContent', message)
-                
-            },                
+
+            toggleMenu(className) {
+                this.activeMenu = className;
+            },
         },
+
+        computed: {
+            computedClasses() {
+                return {
+                    active: this.activeMenu === 'text',
+                }
+            }
+        },
+
         
         beforeDestroy() {
             eventBus.off('newAudioCreated', this.insertAudio());
@@ -106,3 +120,37 @@ import AudioRecorder from '../AudioRecorderComponent.vue';
         },
     }
 </script>
+
+<style>
+.chat-menu-icon-row {
+    background: linear-gradient(#72d0e8, #2f8f96);
+}
+.active {
+    margin-left: 3px;
+    background: linear-gradient(#2f8f96, #72d0e8);
+    border-top-left-radius: 15px;
+    border-top-right-radius: 15px;
+    border-bottom: 2px;
+    border-bottom: 1px solid white;
+}
+
+.active-enter-active,
+.active-leave-active {
+  transition: opacity 0.5s, transform 0.5s ease;
+}
+
+.active-enter, .active-leave-active {
+  opacity: 0;
+  transform: translateY(-100px);
+}
+
+.audio-visualizer-container {
+    border: 1px solid #bfbdbd; 
+    box-shadow: inset 0 0 20px #bfbdbd;
+}
+
+.textbox-send-icon {
+    box-shadow: 0 0 10px 1px #5ad15f; 
+    border-radius: 50%;
+}
+</style>
