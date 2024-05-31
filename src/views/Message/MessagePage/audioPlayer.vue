58b370822a66c87b81828a29303acebcf66c13f7
pistii <pilisi.istvan@gmail.com>
2024-05-31 19:11:28 +0200

    handle multiple type of source data

--- src/views/Message/MessagePage/audioPlayer.vue
+++ src/views/Message/MessagePage/audioPlayer.vue
@@ -2,8 +2,9 @@
 	<div>
 		<v-menu :close-on-content-click="false">
 			<template v-slot:activator="{ on }">
-				<audio controls>
-					<source :src="audio">
+				<audio  style="width: 270px; height: 50px" controls>
+					<source v-if="!audio.local" :src="blobToUrl(base64ToBlob(audio.fileData, 'audio/wav'))">
+					<source v-else :src="audio.FileToken" >
 				</audio>
 			</template>
 			<v-card>
@@ -15,11 +16,9 @@
 	</div>
 </template>
 
-<script>
-export default {
-	props: 
-		['audio']
-	
-}
+<script setup>
+import { base64ToBlob, blobToUrl, fileToBlob } from '@/utils/common.js';
+const props = defineProps( ['audio'])
+
 </script>
   
