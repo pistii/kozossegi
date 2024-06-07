27af4de4719b86c084849a4a1a7a3f9984b775d9
pistii <pilisi.istvan@gmail.com>
2024-06-07 16:12:40 +0200

    Overlay chat reworked. Using one array to store user info and chat content

--- src/stores/MessageStore.js
+++ src/stores/MessageStore.js
@@ -1,92 +1,149 @@
 import { createStore } from 'vuex';
+import fetchData from '@/stores/server_routes';
+import UserStore from './UserStore';
 
 export default createStore({
   state: {
-    partnerId: 0,
-    partner: [],
-    chatRooms: [],
-    chatContent: [],
-    openedOverlayChats: [],
-    newMessages: [],
-    currentRoomId: 0,
-    currentPage: 1,
-    totalPage: 1,
     emojis: [],
+
+    loading: false,
+    userChatHeads: [],
+    //The first item in the array is the active chat, 
+    //every other items are chat heads next to the messaging chat.
+    //Contains the user if no content for it was loaded. 
+    //And also stores all the data for the chat the object hierarchy looks like follows:
+    /*
+    
+      var chat = {
+        'user': {
+          id: 1511,
+          firstName: 'Test'
+          middleName: null,
+          lastName: 'Elek',
+          avatar: 'sdfcxvblécvklbncfvbjmdffdes'
+        },
+        'data': [{
+          MessageId: 1,
+          AuthorId: 1,
+          chatContentId: 1,
+          message: 'Test message',
+          sentDate: '2023-01-01',
+          Status: 1,
+        },
+        {
+          MessageId: 2,
+          AuthorId: 1,
+          chatContentId: 1,
+          message: 'Test message 1',
+          sentDate: '2023-01-02',
+          Status: 1,
+        }],
+        totalPages: 16,
+        currentPage: 1,
+        roomid: 59
+      };
+    */
   },
   mutations: {
-    setPartnerId(state, partnerId) {
-      state.partnerId = partnerId;
-    },
-    setPartner(state, partner) { //Set the user to active when picked from the message partners
-        state.partner = partner;
-    },
-    setChatRooms(state, room) { 
-      state.chatRooms.unshift(room);
-    },
-    setChatContent(state, content) { //Az egész chatet állítja be
-        state.chatContent = content;
-    },
-    addChatRooms(state, chat) { 
-        state.openedOverlayChats.push(chat);
-    },
-    sendNewMessage(state, content) { //Egy darabot üzenetet tesz a contents-be, ha a felhasználó üzenetet küld valakinek
-      state.chatContent.push(content);
-    },
-    setCurrentRoomId(state, currentRoomId) {
-      state.currentRoomId = currentRoomId;
-    },
-    setCurrentPage(state, currentPage) {
-      state.currentPage = currentPage;
-    },
-    setTotalPage(state, totalPage) {
-      state.totalPage = totalPage;
-    },
     setFrequentlyUsedEmojis(state, emoji) {
       state.emojis = emoji;
     },
-    setNewMessageReceived(state, message) {
-      state.newMessages.push(message);
+    setActiveChat(state, chatContents) {
+      state.activeChat = chatContents;
     },
+    addItemToChat(state, message) {
+      console.log(state.activeChat);
+      state.activeChat.data.push(message);
+    },
+    showLoader(state, show) {
+      state.loading = show;
+    }
   },
   getters: {
-    getPartnerId: (state) => () => {
-      return state.partnerId;
-    },
-    getPartner: (state) => () => {
-        return state.partner;
-    },
-    getChatRooms: (state) => () => {
-      return state.chatRooms;
-    },
-    getChatContent: (state) => () => {
-        return state.chatContent;
-    },
-    getOpenedChatRooms: (state) => () => {
-      return state.openedOverlayChats;
-  },
-    getCurrentRoomId: (state) => () => {
-      return state.currentRoomId;
-    },
-    getCurrentPage: (state) => () => {
-      return state.currentPage;
-    },
-    getTotalPage: (state) => () => {
-      return state.totalPage;
-    },
-    getFrequentlyUsedEmojis: (state) => () => {
+    getFrequentlyUsedEmojis: (state) => {
       return state.emojis;
     },
-    getNewMessages: (state) => () => {
-      return state.newMessages;
+    getActiveChat: (state) => () => {
+      return state.userChatHeads[0]; //First item is the activeChat the other ones the chatHeads.
+    },
+    getChatHeads: (state) => () => {
+      return state.userChatHeads.slice(1); 
+    },
+    getLoadingState: (state) => () => {
+      return state.loading; //Activates when loading the data.
     },
   },
   actions: {
-    //Removes the chatroom from the overlay
+    //Removes user from active chat and from userChatHeads. This way all the data for the user removed.
     removeOpenedChatRoom({commit}, payload) {
       const pid = payload;
-      const index = this.state.openedOverlayChats.findIndex(p => p.id === pid);
+      const index = this.state.userChatHeads.find(p => p.user.id === pid);
       if (index !== -1) {
-        this.state.openedOverlayChats.splice(index, 1);
+        this.state.userChatHeads.splice(index, 1);
+      }
+    },
+
+    
+    switchMessageTab({commit}, user) {
+      var chatRoomToSetActive = this.state.userChatHeads.find(p => p.user.id === user.id);
+      var index = this.state.userChatHeads.indexOf(chatRoomToSetActive);
+      //Remove chatRoom from the heads
+      this.state.userChatHeads.splice(index, 1);
+      //Set the first item to the selected one
+      this.state.userChatHeads.unshift(chatRoomToSetActive);
+    },
+
+
+    //adds user to userChatHeads if it doesn't contains the user
+    async addUser({commit}, user) {
+      this.state.loading = true;
+      const MAX_CHAT_HEADS  = 7;
+
+      var contents = await this.dispatch('fetchMessages', user);
+      //console.log("content: " + JSON.stringify(contents));
+      var chat = {
+        'user': user,
+        'chat': contents
+      };
+
+      //Add the user to the userChatHeads
+      if (!this.state.userChatHeads.some(room => room.user == user)) {
+        this.state.userChatHeads.unshift(chat);
+      }
+
+      if (this.state.userChatHeads.length > MAX_CHAT_HEADS) { //Maximum chatHeads is 5 
+        this.state.userChatHeads.splice(MAX_CHAT_HEADS);
+      }
+      this.state.loading = false;
+    },
+
+    fetchMessages({commit}, user) {
+      if (user !== null) {
+        var response = new Promise( (resolve, reject) => {
+          var data = fetchData.methods.fetchData('GET', 'GetChatContentByUserId', null, UserStore.getters.getUserId(), user.id);
+          resolve(data);
+        });
+        return response;
+      }
+    },
+
+    async loadMoreMessage({commit}, chat) {
+      //console.log("load more msg......");
+      var roomId = chat.roomId;
+      var cp = chat.currentPage;
+      var totalPage = chat.totalPages;
+      if (totalPage >= cp) {
+        chat.currentPage++;
+        //console.log("totalPage: " + totalPage + "cp: " + cp + " | roomid:" + chat.roomId);
+        
+        var response = await fetchData.methods
+        .fetchData('GET', 'GetChatContent', null, roomId, cp);
+        
+        //console.log(response.data);
+        this.state.userChatHeads.find(p => p.chat?.roomId === roomId).chat?.data.unshift(...response.data);
+        //chatRoomToUpdate.chat.data.unshift(...response.data);
+        console.log("contents in mssg: " + this.state.userChatHeads.chat);
+        return response; //Return the messages
       }
     },
   }
