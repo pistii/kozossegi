f7f8a041d40937dbcf3139d67bb7407eb0cbdcfd
pistii <pilisi.istvan@gmail.com>
2024-06-07 16:13:52 +0200

    chat reworked, use vuex store

--- src/components/ChatCard/OverlayChat.vue
+++ src/components/ChatCard/OverlayChat.vue
@@ -1,8 +1,12 @@
 <template>
     <div v-if="isLoggedin()">
-        <ChatCardWidget v-if="getOpenedChatRooms.length>0"
+        <ChatCardWidget 
+        v-if="getLoadingState"
         class=" overlayChatPanel "
         />
+        <div class="chatHeadContainer" v-if="showChatHeads">
+            <ChatHeads />
+        </div>
         <div >
             <OnlineFriendsView />
         </div>
@@ -10,50 +14,50 @@
 </template>
 
 <script>
-import OnlineFriendsView from '@/views/OnlineFriendsView.vue';
-import ChatCardWidget from './ChatCardWidget.vue'
-import { isLoggedin } from '@/utils/auth'
-import { createNewChatRoom } from '@/utils/MessageHelper.js'
-import MessageStore from '@/stores/MessageStore';
+import OnlineFriendsView from '@/components/ChatCard/OnlineFriendsView.vue';
+import ChatCardWidget from './ChatCardWidget.vue';
+import ChatHeads from './ChatHeads.vue';
 
+import { isLoggedin } from '@/utils/auth'
+import MessageStore from '@/stores/MessageStore';
 
 export default {
     data() {
         return {
-            createNewChatRoom,
-            isLoggedin
+            isLoggedin,
         }
     },
     components: {
         OnlineFriendsView,
-        ChatCardWidget
+        ChatCardWidget,
+        ChatHeads,
     },
     computed: {
-        getOpenedChatRooms() {
-            return MessageStore.getters.getOpenedChatRooms();
+        getLoadingState() {
+            return MessageStore.getters.getLoadingState() || MessageStore.getters.getActiveChat();
+        },
+        showChatHeads() {
+            if (MessageStore.getters.getChatHeads().length >= 1) {
+            return true;
+            }
+            return false;
         }
     },
-    methods: {
-        async sendMessage(user) {
-            var createdChatRoom = await createNewChatRoom(user)
-            MessageStore.commit('setOpenedChatRooms', createdChatRoom);
-        }
-    }
 }
 </script>
 
-<style>
+<style scoped>
 .overlayChatPanel {
     position: fixed;
     bottom: 0;
     min-width: 300px;
     right: 350px;
     font-size: 13px;
+    height: 400px;
 }
 
 .chatHeadContainer {
     position: fixed;
-    background-color: green;
     bottom: 0;
     height: 300px;
     min-width: 40px;
