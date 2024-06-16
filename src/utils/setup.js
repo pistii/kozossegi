3504648576380a4f839e9dbdc540e0b7b1df2636
pistii <pilisi.istvan@gmail.com>
2024-06-16 17:15:23 +0200

    setup

--- src/utils/setup.js
+++ src/utils/setup.js
@@ -1,15 +1,21 @@
-import { WatchChat } from "./hub";
+import { WatchChat, WatchNotification } from "./hub";
+import store from '@/stores/UserStore';
+
 
 //The method when triggers on page load or login
 //Called from signin
 export async function loginSetup() {
-    WatchChat((data) => {
-      if (!store.getters.hubContainsId(data.senderId))
-      {
-          store.commit('addSenderId', data.senderId);
-      }
-    });
-  }
+  WatchChat((data) => {
+    if (!store.getters.hubContainsId(data.senderId))
+    {
+        store.commit('addSenderIdTomessageHub', data.senderId);
+    }
+  });
+
+  WatchNotification((data) => {
+      console.log("WatchNoti " + data)
+  });
+}
   
   //This method set back the values to its default state. Useful to clear data before data load
 export async function SetToDefault() {
